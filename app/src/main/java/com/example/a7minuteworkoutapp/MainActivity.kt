199678ccd2cca47a67eb5bcfac4192833323c89a
trsohankumar <sohankumartr@gmail.com>
2021-06-03 08:44:28 +0530

    Created the home screen and remover action bar

--- app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.a7minuteworkoutapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.a7minuteworkoutapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.start.setOnClickListener{
+            Toast.makeText(this,"Here we will start the exercise",Toast.LENGTH_SHORT).show()
+        }
     }
 }

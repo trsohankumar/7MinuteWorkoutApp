4a7a249ba68719bc6700178c782fe5e15511e0ff
trsohankumar <sohankumartr@gmail.com>
2021-06-04 10:17:26 +0530

    created the exercise timer

--- app/src/main/java/com/example/a7minuteworkoutapp/ExerciseActivity.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/ExerciseActivity.kt
@@ -3,13 +3,16 @@ package com.example.a7minuteworkoutapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import android.view.View
 import android.widget.Toast
 import com.example.a7minuteworkoutapp.databinding.ActivityExerciseBinding
 
 class ExerciseActivity : AppCompatActivity() {
     private lateinit var binding: ActivityExerciseBinding
-    private var resetTimer:CountDownTimer?=null
-    private var resetProgress:Int = 0
+    private var restTimer:CountDownTimer?=null
+    private var restProgress:Int = 0
+    private var exerciseTimer:CountDownTimer?=null
+    private var exerciseProgress:Int = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityExerciseBinding.inflate(layoutInflater)
@@ -24,41 +27,74 @@ class ExerciseActivity : AppCompatActivity() {
         binding.toolBarExerciseActivity.setNavigationOnClickListener {
             onBackPressed()
         }
-        setupResetView()
-    }
+        binding.llExerciseView.visibility =View.GONE
+        setupRestView()
 
-    private fun setupResetView(){
-        if(resetTimer != null){
-            resetTimer!!.cancel()
-            resetProgress = 0
+    }
+//Rest view is the view when there is no exercise
+    private fun setupRestView(){
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
         }
 
-        setProgressBar()
+        setRestProgressBar()
+    }
+
+    private fun setupExerciseView(){
+        if(exerciseTimer != null){
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
+        }
+
+        setExerciseProgressBar()
     }
 
 
-    private fun  setProgressBar(){
-        binding.progressBar.progress = resetProgress
-        resetTimer = object : CountDownTimer(10000,1000) {
+    private fun  setRestProgressBar(){
+        binding.restProgressBar.progress = restProgress
+        restTimer = object : CountDownTimer(10000,1000) {
             override fun onTick(millisUntilFinished: Long) {
-               resetProgress++
-                binding.progressBar.progress = 10 - resetProgress
-                binding.tvTimer.text = (10 - resetProgress).toString()
+               restProgress++
+                binding.restProgressBar.progress = 10 - restProgress
+                binding.tvRestTimer.text = (10 - restProgress).toString()
             }
 
             override fun onFinish() {
                 Toast.makeText(this@ExerciseActivity,"Timer has been completed",Toast.LENGTH_SHORT).show()
+                binding.llRestView.visibility = View.GONE
+                binding.llExerciseView.visibility = View.VISIBLE
+                setExerciseProgressBar()
+
             }
             
         }.start()
     }
 
+    private fun setExerciseProgressBar(){
+        binding.exerciseProgressBar.progress = exerciseProgress
+        exerciseTimer = object: CountDownTimer(30000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                exerciseProgress++
+                binding.exerciseProgressBar.progress = 30 - exerciseProgress
+                binding.tvExerciseTimer.text = (30-exerciseProgress).toString()
+            }
+
+            override fun onFinish() {
+                Toast.makeText(this@ExerciseActivity,"Timer has been completed",Toast.LENGTH_SHORT).show()
+            }
+        }.start()
+    }
 
 
     public override fun onDestroy() {
-        if(resetTimer != null){
-            resetTimer!!.cancel()
-            resetProgress = 0
+        if(restTimer != null){
+            restTimer!!.cancel()
+            restProgress = 0
+        }
+        if(exerciseTimer != null){
+            exerciseTimer!!.cancel()
+            exerciseProgress = 0
         }
         super.onDestroy()
     }

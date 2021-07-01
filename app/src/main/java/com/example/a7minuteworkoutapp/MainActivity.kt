d73b13248d0c7c13cccfd52068bdad52351cd627
trsohankumar <sohankumartr@gmail.com>
2021-07-01 09:20:53 +0530

    Added the bmi calculator

--- app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt
+++ app/src/main/java/com/example/a7minuteworkoutapp/MainActivity.kt
@@ -18,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, ExerciseActivity::class.java)
             startActivity(intent)
         }
+
+        binding.llBMI.setOnClickListener{
+            val intent = Intent(this, BMIActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
